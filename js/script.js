ac6221dcbc9581c50a7e123fe3984eb1ee6a64fa
rikytestnet2 <shiro70229@gmail.com>
2024-05-27 18:30:42 +0700

    Capstone-Dicoding

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toogle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+// after Hamburger menu click
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Click Di Luar Sidebar Untuk Menghilangkan Nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+        navbarNav.classList.remove('active');
+})
+
+
+  
