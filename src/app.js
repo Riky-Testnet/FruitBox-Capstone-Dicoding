2474be4b081d208e09656199b2ca75898ab5c450
rikytestnet2 <shiro70229@gmail.com>
2024-06-19 14:19:17 +0700

    done100%

--- src/app.js
+++ src/app.js
@@ -66,6 +66,59 @@ document.addEventListener('alpine:init', () => {
     });
 });
 
+// form validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled= true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function () {
+    for(let i = 0; i < form.elements.length; i++ ) {
+        if(form.elements[i].value.length !== 0) {
+            checkoutButton.classList.remove('disabled');
+            checkoutButton.classList.add('disabled');
+        } else {
+            return false;
+        }
+    }
+
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove('disabled');
+});
+
+// Send data
+checkoutButton.addEventListener('click', async function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objData = Object.fromEntries(data);
+    // const message = formatMessage(objData);
+    // window.open('http://wa.me/6282297431860?text=' + encodeURIComponent(message));
+    // Request Snap Token
+    try {
+        const response = await fetch('php/placeOrder.php', {
+            method: 'POST',
+            body: data,
+        });
+        const token = await response.text();
+        window.snap.pay(token);
+    } catch(err) {
+        console.log(err.message);
+    }
+
+});
+
+const formatMessage = (obj) => {
+    return `data Customer
+        Nama: ${obj.name}
+        Email: ${obj.email}
+        Phone: ${obj.phone}
+        Data Pesanan
+        ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n `)}
+        TOTAL: ${rupiah(obj.total)}
+        Terima Kasih`;
+}
+
 // Konversri Ke Rp
 const rupiah = (number) => {
     return new Intl.NumberFormat('id-ID', {
@@ -74,3 +127,7 @@ const rupiah = (number) => {
         minimumFractionDigits: 0,
     }).format(number);
 };
+
+
+
+
